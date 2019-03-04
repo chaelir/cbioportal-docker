4c4c20be53e09cc3064dd46cf2d5a642cccb749e
Charlie Xia <lxia.usc@gmail.com>
2019-03-03 23:53:06 -0800

    add a newly localized Dockerfile

--- cbioportal1.init.sh
+++ cbioportal1.init.sh
@@ -8,10 +8,10 @@ my_configure_file="${HOME}/setup/cbioportal-docker/portal.properties.cbioportal1
 
 ### current configurable portal.properties variables ###
 # for Bash compatibility, changed variable names x.y to x_y
-db_user='cbio1'
-db_password='P@ssword1'
-db_host='cbioDB1'
-portal_db_name='cbioportal1'
+db_user="cbio1"
+db_password="P@ssword1"
+db_host="cbioDB1"
+portal_db_name="cbioportal1"
 sed -i -e "s/db.user=.*/db.user=${db_user}/g" ${my_configure_file}
 sed -i -e "s/db.password=.*/db.password=${db_password}/g" ${my_configure_file}
 sed -i -e "s/db.host=.*/db.host=${db_host}/g" ${my_configure_file}
@@ -19,41 +19,48 @@ sed -i -e "s/portal.db.name=.*/portal.db.name=${portal_db_name}/g" ${my_configur
 
 ### current configurable none portal.properties variables ###
 docker_network='cbio-net1'
-docker_cbio_source='https://github.com/thehyve/cbioportal-docker.git'
-docker_cbio_image='cbioportal-image1'
+docker_cbio_source='https://github.com/chaelir/cbioportal-docker.git'
+docker_cbio_image='cbioportal-v1.17'
 docker_cbio_instance=
 docker_cbio_port=8881
-
-exit
+docker_db_wait=10
+db_dataseed_path="${HOME}/setup/datahub/seedDB"
+db_dataseed_sql="seed-cbioportal_hg19_v2.6.0.sql.gz"
+db_runtime_path="${HOME}/setup/cbioportal-docker-runtime"
 
 ### create docker network ###
-docker network create ${docker_network}
+# create a network if not existing, other do not panic
+docker network create ${docker_network} || true
 
 ### run mysql docker ###
+mkdir ${db_runtime_path}/${db_host} || true
+docker rm -f ${db_host} || true
 docker run -d --restart=always \
-  --name=run-mysql \
+  --name=${db_host} \
   --net=${docker_network} \
   -e MYSQL_ROOT_PASSWORD="${db_password}" \
   -e MYSQL_USER=${db_user} \
   -e MYSQL_PASSWORD="${db_password}" \
   -e MYSQL_DATABASE=${portal_db_name} \
-  -v /${db_runtime_path}/${db_host}:/var/lib/mysql/ \
+  -v ${db_runtime_path}/${db_host}:/var/lib/mysql/ \
   mysql:5.7
 
 ### run mysql with seed database ###
+sleep ${docker_db_wait} ## wait the db to initialize
 docker run \
-  --name=load-seeddb \
   --net=${docker_network} \
   -e MYSQL_USER=${db_user} \
   -e MYSQL_PASSWORD=${db_password} \
   -v ${db_dataseed_path}/cgds.sql:/mnt/cgds.sql:ro \
   -v ${db_dataseed_path}/${db_dataseed_sql}:/mnt/seed.sql.gz:ro \
   mysql:5.7 \
-  sh -c "cat /mnt/cgds.sql | mysql -h${db_host} -u'$MYSQL_USER' -p'$MYSQL_PASSWORD' ${portal_db_name} \
-      && zcat /mnt/seed.sql.gz |  mysql -h${db_host} -u'$MYSQL_USER' -p'$MYSQL_PASSWORD' ${portal_db_name}"
+  sh -c "cat /mnt/cgds.sql | mysql -h${db_host} -u${db_user} -p${db_password} ${portal_db_name} \
+      && zcat /mnt/seed.sql.gz |  mysql -h${db_host} -u${db_user} -p${db_password} ${portal_db_name}"
 
 ### build cbioportal docker image
-docker build -t ${docker_image} https://github.com/thehyve/cbioportal-docker.git
+docker build -t ${docker_cbio_image} ${docker_cbio_source}
+
+exit
 
 ### migrate db (optional) ###
 #docker run --rm -it --net cbio-net \
