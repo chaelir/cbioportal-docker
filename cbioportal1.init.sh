caea7ec169f23ad7e6428a5653b03b3925088a82
Charlie Xia <lxia.usc@gmail.com>
2019-03-05 11:51:34 -0800

    merged original master

--- cbioportal1.init.sh
+++ cbioportal1.init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # turn on echoing of each ran command
 set -x
+stage=$1
+if [ -z $1 ]; then echo "input a stage: run_mysql, build_cbio, or run_cbio"; exit; fi
 
 ### define the portal.properties file to be configured by this init script ###
 # for Bash compatibility, use "${var}" instead of '$var' or '${var}'
-portal_configure_file="${HOME}/setup/cbioportal-docker/portal.properties.cbioportal1"
+# must use absolute path here to be mounted by docker
+portal_configure_file="$HOME/setup/cbioportal-docker/portal.properties.cbioportal1"
 
 ### current configurable portal.properties variables ###
 # for Bash compatibility, changed variable names x.y to x_y
@@ -24,7 +27,7 @@ docker_cbio_image="cbioportal-v1.17"
 docker_cbio_instance="cbioPortal1"
 docker_cbio_dockerfile="Dockerfile.v1.17"
 docker_cbio_port=8881
-docker_cbio_opt="'-Xms2g -Xmx4g'" #important to keep single quote
+docker_cbio_opt="\"-Xms2g -Xmx4g\"" #important to keep single quote
 docker_db_wait=10
 db_dataseed_path="${HOME}/setup/datahub/seedDB"
 db_dataseed_sql="seed-cbioportal_hg19_v2.6.0.sql.gz"
@@ -35,33 +38,39 @@ db_runtime_path="${HOME}/setup/cbioportal-docker-runtime"
 docker network create ${docker_network} || true
 
 ### run mysql docker ###
-mkdir ${db_runtime_path}/${db_host} || true
-docker rm -f ${db_host} || true
-docker run -d --restart=always \
-  --name=${db_host} \
-  --net=${docker_network} \
-  -e MYSQL_ROOT_PASSWORD="${db_password}" \
-  -e MYSQL_USER=${db_user} \
-  -e MYSQL_PASSWORD="${db_password}" \
-  -e MYSQL_DATABASE=${portal_db_name} \
-  -v ${db_runtime_path}/${db_host}:/var/lib/mysql/ \
-  mysql:5.7
-
-### run mysql with seed database ###
-sleep ${docker_db_wait} ## wait the db to initialize
-docker run \
-  --net=${docker_network} \
-  -e MYSQL_USER=${db_user} \
-  -e MYSQL_PASSWORD=${db_password} \
-  -v ${db_dataseed_path}/cgds.sql:/mnt/cgds.sql:ro \
-  -v ${db_dataseed_path}/${db_dataseed_sql}:/mnt/seed.sql.gz:ro \
-  mysql:5.7 \
+if [ $stage == 'run_mysql' ]; then
+  mkdir ${db_runtime_path}/${db_host} || true
+  docker rm -f ${db_host} || true
+  docker run -d --restart=always \
+    --name=${db_host} \
+    --net=${docker_network} \
+    -e MYSQL_ROOT_PASSWORD="${db_password}" \
+    -e MYSQL_USER=${db_user} \
+    -e MYSQL_PASSWORD="${db_password}" \
+    -e MYSQL_DATABASE=${portal_db_name} \
+    -v ${db_runtime_path}/${db_host}:/var/lib/mysql/ \
+    mysql:5.7
+  ### run mysql with seed database ###
+  sleep ${docker_db_wait} ## wait the db to initialize
+  docker run \
+    --net=${docker_network} \
+    -e MYSQL_USER=${db_user} \
+    -e MYSQL_PASSWORD=${db_password} \
+    -v ${db_dataseed_path}/cgds.sql:/mnt/cgds.sql:ro \
+    -v ${db_dataseed_path}/${db_dataseed_sql}:/mnt/seed.sql.gz:ro \
+    mysql:5.7 \
   sh -c "cat /mnt/cgds.sql | mysql -h${db_host} -u${db_user} -p${db_password} ${portal_db_name} \
       && zcat /mnt/seed.sql.gz |  mysql -h${db_host} -u${db_user} -p${db_password} ${portal_db_name}"
+  echo "Take Note: access the mysql db with the following command:"
+  echo "docker exec -it ${db_host} /bin/bash -c \"mysql -h${db_host} -u${db_user} -p${db_password} ${portal_db_name}\""
+fi
+# docker exec -it cbioDB1 /bin/bash -c "mysql -hcbioDB1 -ucbio1 -pP@ssword1 cbioportal1"
 
 ### build cbioportal docker image
+if [ $stage == 'build_cbio' ]; then
 # adding --no-cache is important to avoid cannot fetch errors from apt-get
-docker build --no-cache -t ${docker_cbio_image} -f ${docker_cbio_dockerfile} ${docker_cbio_source}
+	docker build --no-cache -t ${docker_cbio_image} -f ${docker_cbio_dockerfile} ${docker_cbio_source}
+fi
 
 ### migrate db (optional) ###
 #docker run --rm -it --net cbio-net \
@@ -70,11 +79,14 @@ docker build --no-cache -t ${docker_cbio_image} -f ${docker_cbio_dockerfile} ${d
 #    migrate_db.py -p /cbioportal/portal.properties -s /cbioportal/db-scripts/src/main/resources/migration.sql
 
 ### run cbio portal service ###
-docker run -d --restart=always \
-    --name=${docker_cbio_instance} \
-    --net=${docker_network} \
-    -v ${portal_configure_file}:/cbioportal/portal.properties:ro \
-    -e CATALINA_OPTS=${docker_cbio_opt}  \
-    -p ${docker_cbio_port}:8080 \
+if [ $stage == 'run_cbio' ]; then
+  docker rm -f ${docker_cbio_instance} || true
+  docker run -d \ #--restart=always \
+      --name=${docker_cbio_instance} \
+      --net=${docker_network} \
+      -v ${portal_configure_file}:/cbioportal/portal.properties:ro \
+      -e CATALINA_OPTS="${docker_cbio_opt}"  \
+      -p ${docker_cbio_port}:8080 \
 		${docker_cbio_image}
-
+fi
+# docker exec -it cbioPortal1 /bin/bash -c ""
