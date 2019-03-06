b294c3f2edd8d513d682a0fdcd6644af20ce6962
Charlie Xia <lxia.usc@gmail.com>
2019-03-05 17:10:31 -0800

    notes on load public data

--- cbioportal1.init.sh
+++ cbioportal1.init.sh
@@ -2,14 +2,17 @@
 # turn on echoing of each ran command
 set -x
 stage=$1
-if [ -z $1 ]; then echo "input a stage: run_mysql, pop_mysql, build_cbio, or run_cbio"; exit; fi
+if [ -z $stage ]; then echo "input a stage: run_mysql, pop_mysql, build_cbio, or run_cbio"; exit; fi
 
 ### define the portal.properties file to be configured by this init script ###
 # for Bash compatibility, use "${var}" instead of '$var' or '${var}'
 # must use absolute path here to be mounted by docker
 portal_configure_template="$HOME/setup/cbioportal-docker/portal.properties"
 portal_configure_file="$HOME/setup/cbioportal-docker/portal.properties.cbioportal1"
+docker_template="$HOME/setup/cbioportal-docker/Dockerfile"
+docker_file="$HOME/setup/cbioportal-docker/Dockerfile.v1.17"
 cp -f ${portal_configure_template} ${portal_configure_file}
+cp -f ${docker_template} ${docker_file}
 
 ### current configurable portal.properties variables ###
 # for Bash compatibility, changed variable names x.y to x_y
@@ -28,9 +31,9 @@ sed -i -e "s/db.portal_db_name=.*/db.portal_db_name=${db_portal_db_name}/g" ${po
 
 ### current configurable none portal.properties variables ###
 docker_timezone="America/Los_Angeles"
-docker_restart="no"
+docker_restart="always"
 docker_network="cbio-net1"
-docker_cbio_source="https://github.com/chaelir/cbioportal-docker.git" #currently at v1.17
+docker_cbio_source="." #currently at v1.17
 docker_cbio_image="cbioportal-v1.17"
 docker_cbio_instance="cbioPortal1"
 docker_cbio_dockerfile="Dockerfile.v1.17" #this points to tags in 
@@ -43,6 +46,10 @@ db_dataseed_sql="seed-cbioportal_hg19_v2.6.0.sql.gz"
 db_runtime_path="${HOME}/setup/cbioportal-docker-runtime"
 db_datahub_path="$HOME/setup/datahub"
 db_datahub_priv_path="$HOME/setup/datahub_priv"
+db_public_studies=('coadread_tcga')
+
+### quit to show parameters only ###
+if [ $stage == "dry" ]; then exit; fi
 
 ### create docker network ###
 # create a network if not existing, other do not panic
@@ -61,8 +68,6 @@ if [ $stage == 'run_mysql' ]; then
     -e MYSQL_PASSWORD="${db_password}" \
     -e MYSQL_DATABASE="${db_portal_db_name}" \
     -v ${db_runtime_path}/${db_host}:/var/lib/mysql/ \
-    -v ${db_dataseed_path}/cgds.sql:/mnt/cgds.sql:ro \
-    -v ${db_dataseed_path}/${db_dataseed_sql}:/mnt/seed.sql.gz:ro \
     -v ${db_datahub_path}/:/mnt/datahub/ \
     -v ${db_datahub_priv_path}/:/mnt/datahub_priv/ \
     mysql:5.7
@@ -102,7 +107,6 @@ fi
 #    migrate_db.py -p /cbioportal/portal.properties -s /cbioportal/db-scripts/src/main/resources/migration.sql
 
 ### run cbio portal service ###
-echo ${docker_cbio_opt}
 if [ $stage == 'run_cbio' ]; then
   docker rm -f ${docker_cbio_instance} || true
   docker run -d --restart=${docker_restart} \
@@ -111,8 +115,19 @@ if [ $stage == 'run_cbio' ]; then
     -e TZ="${docker_timezone}" \
     -e CATALINA_OPTS='${docker_cbio_opt}' \
     -v ${portal_configure_file}:/cbioportal/portal.properties:ro \
+    -v ${db_datahub_path}/:/mnt/datahub/ \
+    -v ${db_datahub_priv_path}/:/mnt/datahub_priv/ \
     -p ${docker_cbio_port}:8080 \
 		${docker_cbio_image}
 fi
 # docker exec -it cbioPortal1 /bin/bash ""
 # docker logs cbioPortal1
+
+### load cbio database ###
+if [ $stage == 'load_cbio' ]; then
+  for study in ${db_public_studies[@]}; do
+    docker exec -it ${docker_cbio_instance} \
+      metaImport.py -u http://localhost:8080/cbioportal \
+      -s /mnt/datahub/public/${study} -o
+  done
+fi
