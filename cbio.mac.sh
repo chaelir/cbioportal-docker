13522c897a3d36162cb463b757546a4dee7f11d6
Charlie Xia <lxia.usc@gmail.com>
2019-05-20 09:54:10 -0700

    Update cbio.mac.sh

--- cbio.mac.sh
+++ cbio.mac.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-#mkdir -p $HOME/setup
-#cd  $HOME/setup
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#brew install git || brew upgrade git
-#brew install git-lfs || brew upgrade git-lfs
-#git lfs install
-#
-#git clone https://github.com/chaelir/datahub.git || true
-#pushd datahub
-#git checkout cbiov2.1.0
-#popd
-#
-#git clone https://github.com/chaelir/cbioportal-docker.git || true
-#pushd cbioportal-docker
-#git checkout cbiov2.1.0
+
+### NOTE: this requires pre-installed
+### homebrew, docker and xcode-tools
+
+mkdir -p $HOME/setup
+cd  $HOME/setup
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git || brew upgrade git
+brew install git-lfs || brew upgrade git-lfs
+git lfs install
+
+git clone https://github.com/chaelir/datahub.git || true
+pushd datahub
+git checkout cbiov2.1.0
+popd
+
+git clone https://github.com/chaelir/cbioportal-docker.git || true
+pushd cbioportal-docker
+git checkout cbiov2.1.0
 
 ./cbio.deploy.sh create_network release-2.1.0 | bash
 ./cbio.deploy.sh run_mysql release-2.1.0 | bash
@@ -23,4 +27,4 @@
 ./cbio.deploy.sh populate_cbio release-2.1.0 | bash
 ./cbio.deploy.sh rerun_cbio release-2.1.0 | bash
 
-#open brower at http://localhost:8882/cbioportal/ to view
+#Now: open brower at http://localhost:8882/cbioportal/ to view the portal
